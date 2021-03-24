39405ad07f151e377f5bacb4dfde9fc4e523cfa8
Willy <willy@broobe.com>
2021-03-23 22:22:11 -0300

    comienzo de proyecto nextJs

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Hola nav</h1>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layouts/Layout'
+const index = () => {
+    return (
+        <Layout>
+            <h1>Hello world</h1>
+        </Layout>
+    )
+}
+
+export default index
